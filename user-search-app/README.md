4e3dfff95a9fb02040fda8e1a3f90f48979e2294
Zineb Boutaa <boutaazineb08@gmail.com>
2021-09-28 21:36:18 +0100

    Upload user search app

---
+++ user-search-app/README.md
@@ -0,0 +1,37 @@
+# GitHub user search app 🐼.
+
+![Design preview for the GitHub user search app coding challenge](Design/preview.jpg)
+
+## Welcome! 👋
+
+Thanks you for checking my solution.
+
+## Table of contents
+
+- [The challenge](#the-challenge)
+ -[Update record](#Update-record)
+- [live-site](#Live-site)
+- [Author](#author)
+
+## The challenge
+
+The challenge is to build out this GitHub user search app using the [GitHub users API](https://docs.github.com/en/rest/reference/users#get-a-user) and get it looking as close to the design as possible.
+
+The users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Search for GitHub users by their username
+- See relevant user information based on their search
+- Switch between light and dark themes
+- Have the correct color scheme chosen for them based on their computer preferences.
+
+
+## Live-site
+
+[Live site url](https://zineb-bou.github.io/user-search-app./)
+
+## Author
+
+- Website - [Zineb Boutaa](https://zineb-bou.github.io/)
+- Twitter - [@ZBoutaa](https://twitter.com/ZBoutaa)
---
+++ user-search-app/script.js
@@ -0,0 +1,181 @@
+// Variables
+var dark_btn = document.querySelector('.dark_btn');
+var light_btn = document.querySelector('.light_btn');
+
+dark_btn.addEventListener('click', function () {
+  document.body.classList.remove('light_theme');
+  document.body.classList.add('dark_theme');
+  dark_btn.style.visibility = 'hidden';
+  light_btn.style.visibility = 'visible';
+  // Add bg_transition class to the body to implement transition when switching between themes
+  document.body.classList.add('bg_transition');
+});
+light_btn.addEventListener('click', function () {
+  document.body.classList.remove('dark_theme');
+  document.body.classList.add('light_theme');
+  light_btn.style.visibility = 'hidden';
+  dark_btn.style.visibility = 'visible';
+  // Add bg_transition class to the body to implement transition when switching between themes
+  document.body.classList.add('bg_transition');
+});
+// variables
+const form = document.getElementById('myForm');
+
+const main = document.querySelector('.main');
+
+let avatar = document.querySelector('#avatar');
+
+let search_btn = document.querySelector('#search_btn');
+
+let name = document.querySelector('#name');
+
+let user_name = document.querySelector('#user-name');
+
+let join_date = document.querySelector('#join-date');
+
+let bio = document.querySelector('#bio');
+
+let nbr_repo = document.querySelector('#nbr-repo');
+
+let nbr_followers = document.querySelector('#nbr-followers');
+
+let nbr_following = document.querySelector('#nbr-following');
+
+let loc = document.querySelector('#loc');
+
+let twitter_user_name = document.querySelector('#twitter-user-name');
+
+let blog_url = document.querySelector('#blog-url');
+
+let company = document.querySelector('#company');
+
+let no_results = document.querySelector('.no-results-tag');
+
+//Handle the event lisntener when submiting the search
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  let search = document.getElementById('search-input').value;
+  //remove space from search input value
+  let originalName = search.split(' ').join('');
+  // ...send the form data and get a response asynchronously
+  fetch('https://api.github.com/users/' + originalName)
+    .then((result) => {
+      if (result.ok) {
+        return result.json();
+      } else {
+        return Promise.reject({
+          status: result.status,
+          statusText: result.statusText,
+        });
+      }
+    })
+    .then((data) => {
+      //make the no_result tag invisible if the previous request was error
+      no_results.classList.remove('make_it_visible');
+      //Remove the erro styling if the previous request was error
+      myForm.classList.remove('error');
+      //fetch all the user data into data variable .
+      avatar.src = data.avatar_url;
+
+      //user full name
+      if (data.name == null) {
+        name.innerHTML = `I am anonymous 😕`;
+      } else {
+        name.innerHTML = data.name;
+      }
+
+      //  user github account
+      user_name.innerHTML = `@${data.login}`;
+      user_name.href = `https://github.com/${data.login}`;
+
+      // join date
+
+      //Slice year
+      let year = data.created_at.slice(0, 4);
+      //Slice month convert number into letters.
+      let month = convert_month_to_letters(data.created_at.slice(5, 7));
+      //Slice the day
+      let day = data.created_at.slice(8, 10);
+
+      //concatenate year/ month / day, then disply the join date .
+      join_date.innerHTML = `Joined ${year} ${month} ${day}`;
+
+      // bio section
+      if (data.bio == null) {
+        bio.innerHTML = `Oooops !! I didn't add a bio yet. `;
+      } else bio.innerHTML = data.bio;
+
+      // Profile data section
+      nbr_repo.innerHTML = data.public_repos;
+
+      nbr_followers.innerHTML = data.followers;
+
+      nbr_following.innerHTML = data.following;
+
+      // Social links section
+      if (data.location == null) {
+        loc.innerHTML = 'Not aviable.';
+        loc.style.opacity = '0.5';
+      } else {
+        loc.innerHTML = data.location;
+      }
+      //twitter link
+      if (data.twitter_username == null) {
+        twitter_user_name.innerHTML = 'Not aviable.';
+        twitter_user_name.style.opacity = '0.5';
+      } else {
+        twitter_user_name.innerHTML = data.twitter_username;
+        twitter_user_name.href = `https://twitter.com/${data.twitter_username}`;
+      }
+
+      // company working at
+      if (data.company == null) {
+        company.innerHTML = 'Not aviable.';
+        company.style.opacity = '0.5';
+      } else {
+        company.innerHTML = data.company;
+      }
+
+      // website url (slice the link display only 10 char for UI purpore)
+      if (data.blog == '') {
+        blog_url.innerHTML = 'Not aviable.';
+        blog_url.style.opacity = '0.5';
+      } else {
+        blog_url.innerHTML = `${data.blog.slice(0, 20)}... `;
+        blog_url.href = `${data.blog}`;
+      }
+    })
+    //handle the error
+    .catch(function () {
+      display_noResult();
+    });
+});
+
+//Function to convert months from numbers into letters.
+
+function convert_month_to_letters(month) {
+  //convert month to number
+  let month_nbr = Number(month);
+  let months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nev',
+    'Dec',
+  ];
+  return months[month_nbr - 1];
+}
+
+//function that make the no_result tag visible
+function display_noResult() {
+  no_results.classList.add('make_it_visible');
+  myForm.classList.add('error');
+}
